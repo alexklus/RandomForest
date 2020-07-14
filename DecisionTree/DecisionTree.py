d7bfd35a91491ff3538f0582ea118168d3b96abe
alexklus <55231756+alexklus@users.noreply.github.com>
2020-07-14 15:16:13 +0300

    modify

--- DecisionTree/DecisionTree.py
+++ DecisionTree/DecisionTree.py
@@ -19,10 +19,40 @@ def load_data():
 
 
 def train_test_split(df, test_size):
+
+    if isinstance(test_size, float):
+        test_size = round(test_size * len(df))
+
     indices = df.index.tolist()
     test_indices = random.sample(population=indices, k=test_size)
     test_df = df.loc[test_indices]
     train_df = df.drop(test_indices)
-    return train_df,test_df
+
+    return train_df, test_df
 
 
+def check_purity(data):
+    label_column = data[:, -1]
+    unique_classes = np.unique(label_column)
+
+    if len(unique_classes) == 1:
+        return True
+    else:
+        return False
+
+
+def classify_data(data):
+
+    label_column = data[:, -1]
+    unique_classes, count_unique_classes = np.unique(label_column, return_counts=True)
+
+    index = count_unique_classes.argmax()
+    classification = unique_classes[index]
+    return classification
+
+
+df = load_data()
+random.seed(0)
+df_train, df_test = train_test_split(df, 0.2)
+data = df_train.values
+print(classify_data(data))
