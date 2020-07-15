d482036c89fd5853a455c1181182c62c81fbfb8c
alex <alexklu93@gmail.com>
2020-07-15 11:36:28 +0300

    test

--- test.py
+++ test.py
@@ -1,6 +1,3 @@
 import configparser
 
-config = configparser.ConfigParser()
-config["Default"] = {'Data':'../Data/iris.csv'}
-with open('config.ini','w') as configfile:
-    config.write(configfile)
+print("test")
