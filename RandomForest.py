c935331486c558878b685f9086b015d84252a2f3
alex <alexklu93@gmail.com>
2020-07-23 17:42:24 +0300

    update random forest

--- RandomForest.py
+++ RandomForest.py
@@ -1,5 +1,11 @@
+from builtins import print
+
 import numpy as np
 import pandas as pd
+import configparser
+
+from numpy import long
+
 from DecisionTree.DecisionTree import decision_tree_algorithm, train_test_split, calculate_accuracy, \
     decision_tree_predictions, print_tree
 
@@ -30,14 +36,59 @@ def random_forest_predictions(test_df, forest):
     return df_predictions
 
 
+def load_and_prepare_data(data_set_name):
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+
+    df = pd.read_csv(config[data_set_name]['data'])
+    unnecessary_features = config[data_set_name]["unnecessary_features"].split(", ")
+
+    if unnecessary_features[0] == "None":
+        unnecessary_features = []
+
+    label = config[data_set_name]['label']
+    df["label"] = df[label]
+
+    unnecessary_features.append(label)
+    df = df.drop(unnecessary_features, axis=1)
+    df = handle_missing_data(df)
+    return df
 
 
+def handle_missing_data(df):
+    feature_types = determine_type_of_feature(df)
+    i = 0
+    for column in df.columns:
+        if feature_types[i] == "categorical":
+            fill_value = df[column].mode()[0]
+            df = df.fillna({column: fill_value})
+        else:
+            fill_value = df[column].median()
+            df = df.fillna({column: fill_value})
+            i += 1
+    return df
 
-# df = pd.read_csv("./Data/iris.csv")
-# train_df, test_df = train_test_split(df, test_size=0.2)
-# forest = random_forest_algorithm(train_df, n_trees=4, n_bootstrap=80, n_features=4, df_max_depth=6)
-# pred = random_forest_predictions(test_df, forest)
-#
-# for i in range(len(forest)):
-#     print("Tree " + str(i+1))
-#     print_tree(forest[i])
+
+def determine_type_of_feature(df):
+    feature_types = []
+    n_unique_values_threshold = 15
+    for column in df.columns:
+        unique_values = df[column].unique()
+        example_value = unique_values[0]
+
+        if isinstance(example_value, str) or (len(unique_values) <= n_unique_values_threshold):
+            feature_types.append("categorical")
+        else:
+            feature_types.append("continuous")
+    return feature_types
+
+
+df = load_and_prepare_data("Iris")
+
+train_df, test_df = train_test_split(df, test_size=0.2)
+forest = random_forest_algorithm(train_df, n_trees=4, n_bootstrap=500, n_features=4, df_max_depth=6)
+prediction = random_forest_predictions(test_df, forest)
+
+for i in range(len(forest)):
+    print("Tree " + str(i + 1))
+    print_tree(forest[i])
