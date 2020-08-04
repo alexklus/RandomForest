79c78874babd9c4875aff928d44c2a683adc3dbc
alex <alexklu93@gmail.com>
2020-08-04 10:15:36 +0300

    random forest changes

--- randomForest.py
+++ randomForest.py
@@ -4,10 +4,11 @@ import numpy as np
 import pandas as pd
 import configparser
 
-from numpy import long
-
 from DecisionTree.DecisionTree import decision_tree_algorithm, train_test_split, calculate_accuracy, \
-    decision_tree_predictions, print_tree
+    decision_tree_predictions, make_predictions, print_tree
+
+config = configparser.ConfigParser()
+config.read("config.ini")
 
 
 def bootstrapping(train_df, n_bootstrap):
@@ -32,24 +33,24 @@ def random_forest_predictions(test_df, forest):
         predictions = decision_tree_predictions(test_df, tree=forest[i])
         df_predictions[column_name] = predictions
     df_predictions = pd.DataFrame(df_predictions)
+    random_forest_predictions = df_predictions.mode(axis=1)[0]
 
-    return df_predictions
+    return random_forest_predictions
 
 
 def load_and_prepare_data(data_set_name):
-    config = configparser.ConfigParser()
-    config.read("config.ini")
-
-    df = pd.read_csv(config[data_set_name]['data'])
+    df = pd.read_csv(config[data_set_name]['path'])
     unnecessary_features = config[data_set_name]["unnecessary_features"].split(", ")
 
     if unnecessary_features[0] == "None":
         unnecessary_features = []
 
     label = config[data_set_name]['label']
+
     df["label"] = df[label]
 
     unnecessary_features.append(label)
+
     df = df.drop(unnecessary_features, axis=1)
     df = handle_missing_data(df)
     return df
@@ -83,12 +84,45 @@ def determine_type_of_feature(df):
     return feature_types
 
 
-df = load_and_prepare_data("Iris")
+class RandomForest:
+    def __init__(self, data_set_name, n_boostrap, n_features, test_size=0.2, n_trees=10, tree_max_depth=3):
+        self.data_set_name = data_set_name
 
-train_df, test_df = train_test_split(df, test_size=0.2)
-forest = random_forest_algorithm(train_df, n_trees=4, n_bootstrap=500, n_features=4, df_max_depth=6)
-prediction = random_forest_predictions(test_df, forest)
+        self.df = load_and_prepare_data(data_set_name)
 
-for i in range(len(forest)):
-    print("Tree " + str(i + 1))
-    print_tree(forest[i])
+        self.train_df, self.test_df = train_test_split(self.df, test_size)
+
+        self.forest = random_forest_algorithm(self.train_df,
+                                              n_trees=n_trees,
+                                              n_bootstrap=n_boostrap,
+                                              n_features=n_features,
+                                              df_max_depth=tree_max_depth)
+        self.test_predictions = None
+        self.accuracy = None
+
+    def test_model(self):
+        self.test_predictions = random_forest_predictions(self.test_df, self.forest)
+        self.accuracy = calculate_accuracy(self.test_predictions, self.test_df.label)
+
+    def prepare_new_data(self, data):
+        label = config[self.data_set_name]['label']
+        data['label'] = 0
+        unnecessary_features = config[self.data_set_name]["unnecessary_features"].split(", ")
+        if unnecessary_features[0] != "None":
+            for unnecessary_feature in unnecessary_features:
+                if unnecessary_feature in data.columns:
+                    data = data.drop(unnecessary_feature, axis=1)
+        return data
+
+    def predict_new_data(self, data):
+        data = handle_missing_data(data)
+        new_predictions = random_forest_predictions(data, self.forest)
+        data['label'] = new_predictions.values
+        data.to_csv(r"result.csv")
+
+    def print_forest(self):
+        i = 1
+        for tree in self.forest:
+            print("Tree " + str(i))
+            i += 1
+            print_tree(tree, "")
